d42a8bdc15ff22c225cc93bdd84d5dcf8bc8a372
parsagholipour <57809920+parsagholipour@users.noreply.github.com>
2021-07-17 21:48:06 +0430

    Getting ready for file extension filter

--- upload.php
+++ upload.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
-$username = 'vue';
-$password = '&E^Wcb7103A+dsa09&@ED12"P)hG7';
+
+$config = [
+    'username' => 'vue',
+    'password' => '&E^Wcb7103A+dsa09&@ED12"P)hG7',
+    'allowed_file_extensions' => [
+        ''
+    ],
+    'allow_htaccess' => false // Important: Always disable or it allows code injection!
+];
+$username = $config['vue'];
+$password = $config['password'];
 
 $_SESSION['message'] ='';
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST) && isset($_POST['logout'])) {
@@ -18,8 +27,8 @@ if (isset($_POST) && isset($_POST['username']) && isset($_POST['password']))
         } else {
             $_SESSION['message'] = 'Username or password is wrong';
 
-            }
-        }else{
+        }
+    }else{
         $_SESSION['message'] ='** Too many attempts Your IP has been blocked **';
     }
 }
@@ -39,10 +48,37 @@ if (isset($_POST) && isset($_FILES['zip']))
 
     $zip = new ZipArchive;
     if ($zip->open($target_Path) === true) {
+        $home_folder = dirname(__FILE__)."/";
+        //make all the folders
+        for($i = 0; $i < $zip->numFiles; $i++)
+        {
+            $OnlyFileName = $zip->getNameIndex($i);
+            $FullFileName = $zip->statIndex($i);
+            if ($FullFileName['name'][strlen($FullFileName['name'])-1] =="/")
+            {
+                @mkdir($home_folder."/".$FullFileName['name'],0700,true);
+            }
+        }
+
+        //unzip into the folders
+        for($i = 0; $i < $zip->numFiles; $i++)
+        {
+            $OnlyFileName = $zip->getNameIndex($i);
+            $FullFileName = $zip->statIndex($i);
+
+            if (!($FullFileName['name'][strlen($FullFileName['name'])-1] =="/"))
+            {
+                if (!preg_match('#\.(php|phtml|gif|png)$#i', $OnlyFileName))
+                {
+                    copy('zip://'. $target_Path .'#'. $OnlyFileName , $home_folder."/".$FullFileName['name'] );
+                }
+            }
+        }
+        $zip->close();
         $zip->extractTo(__DIR__);
         $zip->close();
-         $_SESSION['message'] ='Unzipped Process Successful!';
-         unlink($target_Path);
+        $_SESSION['message'] ='Successfully extracted!';
+        unlink($target_Path);
     } else {
         $_SESSION['message'] = 'Unzipped Process failed';
     }
@@ -56,18 +92,18 @@ if (!empty($_SESSION['message'])){
 
 <?php if(! isset($_SESSION['username'])) {
     ?>
-        <div class="container">
-            <h3>Login</h3>
-            <form class="form-container" action="" method="post">
-                <label>
-                    <input placeholder="username" class="form-control" type="text" name="username">
-                </label>
-                <label>
-                    <input placeholder="password" class="form-control" type="password" name="password">
-                </label>
-                <button class="submit-button" type="submit" >login</button>
-            </form>
-        </div>
+    <div class="container">
+        <h3>Login</h3>
+        <form class="form-container" action="" method="post">
+            <label>
+                <input placeholder="username" class="form-control" type="text" name="username">
+            </label>
+            <label>
+                <input placeholder="password" class="form-control" type="password" name="password">
+            </label>
+            <button class="submit-button" type="submit" >login</button>
+        </form>
+    </div>
 
 <?php } ?>
 
@@ -94,115 +130,116 @@ if (!empty($_SESSION['message'])){
     </div>
 
 <?php } ?>
-<style>
-    * {
-        font-family: monospace;
-    }
+    <style>
+        * {
+            font-family: monospace;
+        }
 
-    .form-container {
-        display: flex;
-        margin: auto;
-        flex-direction: column;
-        width: 50%;
-        justify-content: center;
-        height: 30%;
-    }
-
-    @media only screen and (max-width: 600px) {
         .form-container {
-            width: 90%;
+            display: flex;
+            margin: auto;
+            flex-direction: column;
+            width: 50%;
+            justify-content: center;
+            height: 30%;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .form-container {
+                width: 90%;
+            }
+
+            .container > h3 {
+                font-size:50px!important;
+            }
+        }
+        .submit-button {
+            background: sandybrown;
+            color: #000111;
+            padding: 10px 0px;
+            border: none;
+            outline: none;
+            box-shadow: 0px 0px 6px -1px #4c1010;
+            border-radius: 8px;
+            cursor: pointer;
+            transition: 0.3s all;
+        }
+
+        .submit-button:hover {
+            background: #efac70;
+            box-shadow: 0px 0px 6px 0px #561616;
+        }
+
+        .form-control {
+            margin: 10px 0px;
+            border-radius: 5px;
+            outline: none;
+            width: 100%;
+            min-height: 40px;
+            border: solid 1px #d7eada;
+            transition: border 150ms;
+        }
+
+        .form-control:focus {
+            border: solid 2px #487051;
+        }
+
+        .container {
+            padding-right: 40px;
+            padding-left: 40px;
+        }
+
+        .footer {
+            background: #8fd19e;
+            position:fixed;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        }
+        .footer p {
+            text-align: center;
         }
 
         .container > h3 {
-            font-size:50px!important;
+            margin-top: 60px;
+            text-align: center;
+            font-size: 100px;
         }
-    }
-    .submit-button {
-        background: sandybrown;
-        color: #000111;
-        padding: 10px 0px;
-        border: none;
-        outline: none;
-        box-shadow: 0px 0px 6px -1px #4c1010;
-        border-radius: 8px;
-        cursor: pointer;
-        transition: 0.3s all;
-    }
 
-    .submit-button:hover {
-        background: #efac70;
-        box-shadow: 0px 0px 6px 0px #561616;
-    }
+        .message {
+            color: #f10000;
+            position: absolute;
+            top: 10px;
+            left: 0;
+            right: 0;
+            text-align: center;
+        }
+        .logout {
+            position: absolute;
+            top: 0;
+            left: 0;
+            background: sandybrown;
+            color: #000111;
+            padding: 10px 10px;
+            border: none;
+            outline: none;
+            box-shadow: 0px 0px 6px -1px #4c1010;
+            border-radius: 0px 0px 8px 0px;
+            cursor: pointer;
+            transition: 0.3s all;
+        }
 
-    .form-control {
-        margin: 10px 0px;
-        border-radius: 5px;
-        outline: none;
-        width: 100%;
-        min-height: 40px;
-        border: solid 1px #d7eada;
-        transition: border 150ms;
-    }
-
-    .form-control:focus {
-        border: solid 2px #487051;
-    }
-
-    .container {
-        padding-right: 40px;
-        padding-left: 40px;
-    }
-
-    .footer {
-        background: #8fd19e;
-        position:fixed;
-        bottom: 0;
-        left: 0;
-        right: 0;
-    }
-    .footer p {
-        text-align: center;
-    }
-
-    .container > h3 {
-        margin-top: 60px;
-        text-align: center;
-        font-size: 100px;
-    }
-
-    .message {
-        color: #f10000;
-        position: absolute;
-        top: 10px;
-        left: 0;
-        right: 0;
-        text-align: center;
-    }
-    .logout {
-        position: absolute;
-        top: 0;
-        left: 0;
-        background: sandybrown;
-        color: #000111;
-        padding: 10px 10px;
-        border: none;
-        outline: none;
-        box-shadow: 0px 0px 6px -1px #4c1010;
-        border-radius: 0px 0px 8px 0px;
-        cursor: pointer;
-        transition: 0.3s all;
-    }
-
-    .logout:hover {
-        padding: 13px 13px;
-        background: #efa86b;s
+        .logout:hover {
+            padding: 13px 13px;
+            background: #efa86b;s
         box-shadow: 0px 0px 6px 0px #4c1010;
-    }
-</style>
+        }
+    </style>
 
 
-<!-- ips
-end-->
+    <!-- ips
+127.0.0.1, 1
+    end-->
 
 
 
